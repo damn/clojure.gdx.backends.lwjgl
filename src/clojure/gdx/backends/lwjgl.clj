dd897081e88f098d02da9a4ce073fcc033a14623
Michael Sappler <michaelsappler@gmail.com>
2025-06-05 09:58:56 +0530

    only one namespace

--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -1,47 +1,174 @@
 (ns clojure.gdx.backends.lwjgl
-  (:require [clojure.gdx.backends.lwjgl.utils :as utils])
   (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
-                                             Lwjgl3ApplicationConfiguration)))
+                                             Lwjgl3Application$GLDebugMessageSeverity
+                                             Lwjgl3ApplicationConfiguration
+                                             Lwjgl3ApplicationConfiguration$GLEmulation
+                                             Lwjgl3Graphics$Lwjgl3DisplayMode
+                                             Lwjgl3Graphics$Lwjgl3Monitor
+                                             Lwjgl3WindowConfiguration)))
 
-(defn display-mode
+(defn- display-mode->map [^Lwjgl3Graphics$Lwjgl3DisplayMode display-mode]
+  {:width          (.width        display-mode)
+   :height         (.height       display-mode)
+   :refresh-rate   (.refreshRate  display-mode)
+   :bits-per-pixel (.bitsPerPixel display-mode)
+   :monitor-handle (.getMonitor   display-mode)})
+
+(defn- monitor->map [^Lwjgl3Graphics$Lwjgl3Monitor monitor]
+  {:virtual-x      (.virtualX         monitor)
+   :virtual-y      (.virtualY         monitor)
+   :name           (.name             monitor)
+   :monitor-handle (.getMonitorHandle monitor)})
+
+(defn- map->display-mode [{:keys [width height refresh-rate bits-per-pixel monitor-handle]}]
+  (let [constructor (.getDeclaredConstructor Lwjgl3Graphics$Lwjgl3DisplayMode
+                                             (into-array Class [Long/TYPE Integer/TYPE Integer/TYPE Integer/TYPE Integer/TYPE]))
+        _ (.setAccessible constructor true)]
+    (.newInstance constructor
+                  (into-array Object [monitor-handle width height refresh-rate bits-per-pixel]))))
+
+(defn- map->monitor [{:keys [virtual-x virtual-y name monitor-handle]}]
+  (let [constructor (.getDeclaredConstructor Lwjgl3Graphics$Lwjgl3Monitor
+                                             (into-array Class [Long/TYPE Integer/TYPE Integer/TYPE String]))
+        _ (.setAccessible constructor true)]
+    (.newInstance constructor
+                  (into-array Object [monitor-handle virtual-x virtual-y name]))))
+
+(defn- k->glversion [gl-version]
+  (case gl-version
+    :angle-gles20 Lwjgl3ApplicationConfiguration$GLEmulation/ANGLE_GLES20
+    :gl20         Lwjgl3ApplicationConfiguration$GLEmulation/GL20
+    :gl30         Lwjgl3ApplicationConfiguration$GLEmulation/GL30
+    :gl31         Lwjgl3ApplicationConfiguration$GLEmulation/GL31
+    :gl32         Lwjgl3ApplicationConfiguration$GLEmulation/GL32))
+
+(defn- set-window-config-key! [^Lwjgl3WindowConfiguration object k v]
+  (case k
+    :initial-visible? (.setInitialVisible object (boolean v))
+    :windowed-mode   (.setWindowedMode object
+                                       (int (:width v))
+                                       (int (:height v)))
+    :resizable? (.setResizable object (boolean v))
+    :decorated? (.setDecorated object (boolean v))
+    :maximized? (.setMaximized object (boolean v))
+    :maximized-monitor (.setMaximizedMonitor object (map->monitor v))
+    :auto-iconify? (.setAutoIconify object (boolean v))
+    :window-position (.setWindowPosition object
+                                         (int (:x v))
+                                         (int (:y v)))
+    :window-size-limits (.setWindowSizeLimits object
+                                              (int (:min-width  v))
+                                              (int (:min-height v))
+                                              (int (:max-width  v))
+                                              (int (:max-height v)))
+    :window-icons (.setWindowIcon object
+                                  ; filetype
+                                  ; array of string of file icons
+                                  )
+
+    :window-listener (.setWindowListener object
+                                         ; Lwjgl3WindowListener v
+                                         )
+
+    :initial-background-color (.setInitialBackgroundColorer object #_(->munge-color v))
+
+    :fullscreen-mode (.setFullscreenMode object (map->display-mode v))
+    :title (.setTitle object (str v))
+    :vsync? (.useVsync object (boolean v))))
+
+(defn- set-application-config-key! [^Lwjgl3ApplicationConfiguration object k v]
+  (case k
+    :audio (.setAudioConfig object
+                            (int (:simultaneous-sources v))
+                            (int (:buffer-size         v))
+                            (int (:buffer-count        v)))
+    :disable-audio? (.disableAudio object (boolean v))
+    :max-net-threads (.setMaxNetThreads object (int v))
+    :opengl-emulation (.setOpenGLEmulation object
+                                           (k->glversion (:gl-version v))
+                                           (int (:gles-3-major-version v))
+                                           (int (:gles-3-minor-version v)))
+    :backbuffer (.setBackBufferConfig object
+                                      (int (:r       v))
+                                      (int (:g       v))
+                                      (int (:b       v))
+                                      (int (:a       v))
+                                      (int (:depth   v))
+                                      (int (:stencil v))
+                                      (int (:samples v)))
+    :transparent-framebuffer (.setTransparentFramebuffer object (boolean v))
+    :idle-fps (.setIdleFPS object (int v))
+    :foreground-fps (.setForegroundFPS object (int v))
+    :pause-when-minimized? (.setPauseWhenMinimized object (boolean v))
+    :pause-when-lost-focus? (.setPauseWhenLostFocus object (boolean v))
+
+    ; String preferencesDirectory, Files.FileType preferencesFileType
+    #_(defmethod set-option! :preferences [object _ v]
+        (.setPreferencesConfig object
+                               (str (:directory v))
+                               ; com.badlogic.gdx.Files.FileType
+                               (k->filetype (:filetype v))))
+
+    ; com.badlogic.gdx.graphics.glutils.HdpiMode/ 'Logical' / 'Pixels'
+    #_(defmethod set-option! :hdpi-mode [object _ v]
+        ; com.badlogic.gdx.graphics.glutils.HdpiMode
+        (.setHdpiMode object (k->hdpi-mode v)))
+
+    #_(defmethod set-option! :gl-debug-output? [object _ v]
+        (.enableGLDebugOutput object
+                              (boolean (:enable? v))
+                              (->PrintStream (:debug-output-stream v))))
+
+    (set-window-config-key! object k v)))
+
+(defn- create-application-config [config]
+  (let [obj (Lwjgl3ApplicationConfiguration.)]
+    (doseq [[k v] config]
+      (set-application-config-key! obj k v))
+    obj))
+
+(defn- create-window-config [config]
+  (let [obj (Lwjgl3WindowConfiguration.)]
+    (doseq [[k v] config]
+      (set-window-config-key! obj k v))
+    obj))
+
+(defn- k->gl-debug-message-severity [k]
+  (case k
+    :high         Lwjgl3Application$GLDebugMessageSeverity/HIGH
+    :medium       Lwjgl3Application$GLDebugMessageSeverity/MEDIUM
+    :low          Lwjgl3Application$GLDebugMessageSeverity/LOW
+    :notification Lwjgl3Application$GLDebugMessageSeverity/NOTIFICATION))
+
+(defn display-mode!
   ([monitor]
-   (utils/display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayMode (utils/map->monitor monitor))))
+   (display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayMode (map->monitor monitor))))
   ([]
-   (utils/display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayMode))))
+   (display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayMode))))
 
-(defn display-modes
-  "The available display-modes of the primary or the given monitor."
+(defn display-modes!
   ([monitor]
-   (map utils/display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayModes (utils/map->monitor monitor))))
+   (map display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayModes (map->monitor monitor))))
   ([]
-   (map utils/display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayModes))))
+   (map display-mode->map (Lwjgl3ApplicationConfiguration/getDisplayModes))))
 
-(defn primary-monitor
-  "the primary monitor."
+(defn primary-monitor!
   []
-  (utils/monitor->map (Lwjgl3ApplicationConfiguration/getPrimaryMonitor)))
+  (monitor->map (Lwjgl3ApplicationConfiguration/getPrimaryMonitor)))
 
-(defn monitors
-  "The connected monitors."
+(defn monitors!
   []
-  (map utils/monitor->map (Lwjgl3ApplicationConfiguration/getMonitors)))
+  (map monitor->map (Lwjgl3ApplicationConfiguration/getMonitors)))
 
-; => listener is part of config and just a bunch of functions ..... with defaults can do
-; * 1. try not to set Gdx global state (need functional alternatives in Gdx ?)
-; * 2. where do I create Gdx ?
-(defn application [config listener]
+(defn application! [config listener]
   (Lwjgl3Application. listener
-                      (utils/create-application-config config)))
+                      (create-application-config config)))
 
-(defn window
-  "Creates a new Lwjgl3Window using the provided listener and Lwjgl3WindowConfiguration. This function only just instantiates a Lwjgl3Window and returns immediately. The actual window creation is postponed with Application.postRunnable(Runnable) until after all existing windows are updated."
-  [application listener config]
+(defn new-window! [application listener config]
   (Lwjgl3Application/.newWindow application
                                 listener
-                                (utils/create-window-config config)))
+                                (create-window-config config)))
 
-(defn set-gl-debug-message-control
-  "Enables or disables GL debug messages for the specified severity level. Returns false if the severity level could not be set (e.g. the NOTIFICATION level is not supported by the ARB and AMD extensions). See Lwjgl3ApplicationConfiguration.enableGLDebugOutput(boolean, PrintStream)"
-  [severity enabled?]
-  (Lwjgl3Application/setGLDebugMessageControl (utils/k->gl-debug-message-severity severity)
+(defn set-gl-debug-message-control! [severity enabled?]
+  (Lwjgl3Application/setGLDebugMessageControl (k->gl-debug-message-severity severity)
                                               (boolean enabled?)))
--- test/clojure/gdx/lwjgl_test.clj
+++ test/clojure/gdx/lwjgl_test.clj
@@ -1,16 +1,15 @@
 (ns clojure.gdx.lwjgl-test
   (:require [clojure.gdx.backends.lwjgl :as lwjgl]))
 
-; TODO FIXME can't call display-mode without setting GLFW async first
-; => its inutils now!
-
 (defn -main []
-  #_(let [display-mode (lwjgl/display-mode)]
-    (println "display-mode: " display-mode)
-    (println "primary monitor: " (lwjgl/primary-monitor))
-    (lwjgl/application {:fullscreen-mode display-mode
-                        :mac-os {:glfw-async? true}}))
+  (.set org.lwjgl.system.Configuration/GLFW_LIBRARY_NAME "glfw_async")
 
-  (lwjgl/application {:mac-os {:glfw-async? true}}
-                     (proxy [com.badlogic.gdx.ApplicationAdapter] []))
+  #_(let [display-mode (lwjgl/display-mode)]
+      (println "display-mode: " display-mode)
+      (println "primary monitor: " (lwjgl/primary-monitor))
+      (lwjgl/application {:fullscreen-mode display-mode
+                          :mac-os {:glfw-async? true}}))
+
+  (lwjgl/application! {}
+                      (proxy [com.badlogic.gdx.ApplicationAdapter] []))
   )
