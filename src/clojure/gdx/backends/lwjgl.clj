7d423b1b77bea83e1b70485d6226ca534a7e2adf
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 03:24:35 +0530

    simplify api namespace even more

--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -1,22 +1,11 @@
 (ns clojure.gdx.backends.lwjgl
-  (:require [clojure.gdx.application.listener :as listener]
-            [clojure.gdx.backends.lwjgl.application :as application]
-            [clojure.gdx.backends.lwjgl.application.configuration :as config]
-            [clojure.gdx.backends.lwjgl.window.configuration :as window-config]))
+  (:require [clojure.gdx.backends.lwjgl.application :as application]
+            [clojure.gdx.backends.lwjgl.application.configuration :as config]))
 
-(def display-mode!    config/display-mode!)
-(def display-modes!   config/display-modes!)
-(def primary-monitor! config/primary-monitor!)
-(def monitors!        config/monitors!)
-
-(defn start-application! [listener config]
-  (application/start! (listener/create listener)
-                      (config/create config)))
-
-(defn new-window! [application listener config]
-  (application/new-window! application
-                           listener
-                           (window-config/create config)))
-
-(defn set-gl-debug-message-control! [severity enabled?]
-  (application/set-gl-debug-message-control! severity enabled?))
+(def start-application!            application/start!)
+(def new-window!                   application/new-window!)
+(def set-gl-debug-message-control! application/set-gl-debug-message-control!)
+(def display-mode!                 config/display-mode!)
+(def display-modes!                config/display-modes!)
+(def primary-monitor!              config/primary-monitor!)
+(def monitors!                     config/monitors!)
--- src/clojure/gdx/backends/lwjgl/application.clj
+++ src/clojure/gdx/backends/lwjgl/application.clj
@@ -1,12 +1,18 @@
 (ns clojure.gdx.backends.lwjgl.application
-  (:require [clojure.gdx.backends.lwjgl.application.gl-debug-message-severity :as gl-debug-message-severity])
+  (:require [clojure.gdx.application.listener :as listener]
+            [clojure.gdx.backends.lwjgl.application.configuration :as config]
+            [clojure.gdx.backends.lwjgl.application.gl-debug-message-severity :as gl-debug-message-severity]
+            [clojure.gdx.backends.lwjgl.window.configuration :as window-config])
   (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application)))
 
-(defn start! [listener configuration]
-  (Lwjgl3Application. listener configuration))
+(defn start! [listener config]
+  (Lwjgl3Application. (listener/create listener)
+                      (config/create config)))
 
-(defn new-window! [application listener configuration]
-  (Lwjgl3Application/.newWindow application listener configuration))
+(defn new-window! [application listener config]
+  (Lwjgl3Application/.newWindow application
+                                listener
+                                (window-config/create config)))
 
 (defn set-gl-debug-message-control!
   "Enables or disables GL debug messages for the specified severity level. Returns false if the severity level could not be set (e.g. the NOTIFICATION level is not supported by the ARB and AMD extensions). See `Lwjgl3ApplicationConfiguration.enableGLDebugOutput(boolean, PrintStream)`."
