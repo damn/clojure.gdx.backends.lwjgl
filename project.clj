7ccd73c2d8753e74cd595a2858bf1fa2f99ecc0a
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 15:08:40 +0530

    Depend on clojure.gdx

--- project.clj
+++ project.clj
@@ -1,16 +1,12 @@
 (def libgdx-version "1.13.5")
 
 (defproject clojure.gdx.backends.lwjgl "1.13.5"
-
-  ; for dev loop
   :repositories [["jitpack" "https://jitpack.io"]]
-
   :dependencies [[org.clojure/clojure "1.12.0"]
+                 [clojure.gdx "1.13.5"]
                  [com.badlogicgames.gdx/gdx-platform       ~libgdx-version :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl3 ~libgdx-version]
-
-                 [com.github.damn/clojure.dev-loop "ef54a03"]
-                 ]
+                 [com.github.damn/clojure.dev-loop "ef54a03"]]
   :plugins [[lein-codox "0.10.8"]]
   :codox {:source-uri "https://github.com/damn/clojure.gdx.backends.lwjgl/blob/main/{filepath}#L{line}"
           :metadata {:doc/format :markdown}}
